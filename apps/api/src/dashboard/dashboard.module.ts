6466ac53bf13335cd1487ea60ca1e2468fecf502
xieyu <523018705@qq.com>
2026-03-02 09:08:01 +0800

    fix(api): import subscriptions module in dashboard

--- apps/api/src/dashboard/dashboard.module.ts
+++ apps/api/src/dashboard/dashboard.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma.module';
+import { SubscriptionsModule } from '../subscriptions/subscriptions.module';
 
 import { DashboardController } from './dashboard.controller';
 import { DashboardService } from './dashboard.service';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, SubscriptionsModule],
   controllers: [DashboardController],
   providers: [DashboardService]
 })
